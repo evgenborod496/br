4e94140887652b3c443905ffb6302b3d315d98f1
evgenborod496 <evgenborod496@gmail.com>
2019-03-24 23:11:42 +0300

    feat: - complete the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let mass = arguments[1];
+  let st = arguments[0];
+  while (st.length > 0) {
+    if (st == reset(st, mass)) {
+      return false;
+    }
+
+    st = reseter(st, mass);
+  }
+
+  return true;
+}
+
+function reset(st, mass) {
+  for (let i = 0; i < st.length - 1; i++) {
+    for (let j = 0; j < mass.length; j++) {
+      if (st[i] == mass[j][0] && st[i + 1] == mass[j][1]) {
+        st = st.slice(0, i) + st.slice(i + 2);
+        return st;
+      }
+    }
+  }
+  return st;
 }
